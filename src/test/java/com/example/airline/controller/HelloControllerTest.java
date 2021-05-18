ca954eabb4e9f3e3f156d598cbada08d6d8a63b0
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-05-18 20:42:40 +0300

    test3 failed

--- src/test/java/com/example/airline/controller/HelloControllerTest.java
+++ src/test/java/com/example/airline/controller/HelloControllerTest.java
@@ -20,6 +20,6 @@ class HelloControllerTest {
     @Test
     public void testHello() throws Exception {
         this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, world!")));
+                .andExpect(content().string(containsString("Hello987, world!")));
     }
 }
