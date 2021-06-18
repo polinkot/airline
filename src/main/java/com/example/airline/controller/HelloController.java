c0dc0fe979c69d153e21d55e766b5ba83d2f117f
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-06-18 15:16:09 +0300

    7777

--- src/main/java/com/example/airline/controller/HelloController.java
+++ src/main/java/com/example/airline/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/6666")
+@RequestMapping("/7777")
 public class HelloController {
 
     @GetMapping
