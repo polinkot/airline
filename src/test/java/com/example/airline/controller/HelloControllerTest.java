09416c66731552e4db4f1473515040ee9f6f32ab
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-06-21 16:19:57 +0300

    111

--- src/test/java/com/example/airline/controller/HelloControllerTest.java
+++ src/test/java/com/example/airline/controller/HelloControllerTest.java
@@ -19,7 +19,7 @@ class HelloControllerTest {
 
     @Test
     public void testHello() throws Exception {
-        this.mockMvc.perform(get("/111")).andDo(print()).andExpect(status().isOk())
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, world! AWS deploy")));
     }
 }
