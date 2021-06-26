d2e9b39481b343352785596ffe4aa0d014ba170f
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-06-26 12:07:06 +0300

    detekt

--- mainApp/src/main/java/com/example/airline/controller/HelloController.kt
+++ mainApp/src/main/java/com/example/airline/controller/HelloController.kt
@@ -11,4 +11,16 @@ class HelloController {
     fun hello(): String {
         return "Hello, world!"
     }
+
+    fun hello111(args: Array<String>, args6: Array<String>, args5: Array<String>, args4: Array<String>, args3: Array<String>,args2: Array<String>,args1: Array<String>, args22: Array<String>, args61: Array<String>, args51: Array<String>, args41: Array<String>, args31: Array<String>,args21: Array<String>,args11: Array<String>) {
+    }
+
+    fun hello1112(args: Array<String>, args6: Array<String>, args5: Array<String>, args4: Array<String>, args3: Array<String>,args2: Array<String>,args1: Array<String>, args22: Array<String>, args61: Array<String>, args51: Array<String>, args41: Array<String>, args31: Array<String>,args21: Array<String>,args11: Array<String>) {
+    }
+
+    fun hello13151(args: Array<String>, args6: Array<String>, args5: Array<String>, args4: Array<String>, args3: Array<String>,args2: Array<String>,args1: Array<String>, args22: Array<String>, args61: Array<String>, args51: Array<String>, args41: Array<String>, args31: Array<String>,args21: Array<String>,args11: Array<String>) {
+    }
+
+    fun hello11561(args: Array<String>, args6: Array<String>, args5: Array<String>, args4: Array<String>, args3: Array<String>,args2: Array<String>,args1: Array<String>, args22: Array<String>, args61: Array<String>, args51: Array<String>, args41: Array<String>, args31: Array<String>,args21: Array<String>,args11: Array<String>) {
+    }
 }
