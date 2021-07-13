8a392ccff26b196eb2decb81cdddf7f2a62a451f
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-07-13 15:32:39 +0300

    wrong6554444

--- mainApp/src/test/kotlin/com/example/airline/controller/HelloControllerTest.kt
+++ mainApp/src/test/kotlin/com/example/airline/controller/HelloControllerTest.kt
@@ -19,7 +19,7 @@ class HelloControllerTest {
         mockMvc.get("/") {
         }.andExpect {
             status { isOk }
-            content { string("Hello5677233333334566, world!") }
+            content { string("Hello56772334566, world!") }
         }
     }
 }
