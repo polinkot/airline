b6bddf502f0e1637162f96e29e095157853fa296
polina.kotelnikova <p.kotelnikova@i-teco.ru>
2021-07-13 16:08:13 +0300

    good

--- mainApp/src/test/kotlin/com/example/airline/controller/HelloControllerTest.kt
+++ mainApp/src/test/kotlin/com/example/airline/controller/HelloControllerTest.kt
@@ -19,7 +19,7 @@ class HelloControllerTest {
         mockMvc.get("/") {
         }.andExpect {
             status { isOk }
-            content { string("Hello,55555 world!") }
+            content { string("Hello, world!") }
         }
     }
 }
